9b63b11f5c9575499fce058b2720075782b4964f
Ayush Mishra <73516326+ayushnvs@users.noreply.github.com>
2022-09-11 16:53:23 +0000

    Added html and bootstrap

--- adhoorasahitya/home/views.py
+++ adhoorasahitya/home/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
 
-# Create your views here.
+contents = [{
+    "title": 'त्वमेव माता चपिता त्वमेव',
+    'collection': 'Poetry'
+}, {
+    'title': 'Father we thank thee',
+    'collection': 'Poetry'
+}, {
+    'title': 'पुष्प की अभिलाषा',
+    'author': 'माखन लाल चतुर्वेदी',
+    'collection': 'Poetry'
+}]
+
+my_dict = {'contents': contents}
+
+
 def home_page(request):
-	return render(request, 'index.html', {})
+    return render(request, 'index.html', my_dict)
